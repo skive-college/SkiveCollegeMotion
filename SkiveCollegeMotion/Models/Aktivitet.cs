1c7c8fae5ab07f4bf79a1757e47ab1235f2d91ce
Oliver Stenkilde <oliv6101@skivecollege.dk>
2019-04-24 10:31:50 +0200

    tilføj aktivitet

---
+++ SkiveCollegeMotion/Models/Aktivitet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+
+namespace SkiveCollegeMotion.Models
+{
+	public class Aktivitet
+    {
+        public int ID { get; set; }
+        public string navn { get; set; }
+        public string sted { get; set; }
+        public int antal { get; set; }
+        public string ansvarlig { get; set; }
+    }
+}
