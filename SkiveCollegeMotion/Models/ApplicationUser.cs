38d7d68e4e8dd51f90b25c3e51fddb930a59eaad
Oliver Stenkilde <oliv6101@skivecollege.dk>
2019-05-07 13:35:17 +0200

    dansk

--- SkiveCollegeMotion/Models/ApplicationUser.cs
+++ SkiveCollegeMotion/Models/ApplicationUser.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel;
 
 namespace SkiveCollegeMotion.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [DisplayName("Brugernavn")]
+        public override string UserName { get => base.UserName; set => base.UserName = value; }
         public string Navn { get; set; }
         public string Hold { get; set; }
         public string ElevType { get; set; }
